78cfcbc92851e9181bc87dc234818c276135b70e
Austin <200422554@student.georgianc.on.ca>
2020-09-20 17:01:33 -0400

    lab 2 completed

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Food()
+        {
+            return View();
+        }
+        public IActionResult Tech()
+        {
+            return View();
+        }
+        public IActionResult Sports()
+        {
+            return View();
+        }
+        public IActionResult Details(string Categories)
+        {
+            ViewBag.Categories = "This is the" + Categories + "Category";
+
+            return View();
+        }
+    }
+}
